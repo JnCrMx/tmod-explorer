c02f0cc3fa7c5c5a5dd9c2f9ed2962cbe94ffbe3
JCM <jacama2009@gmail.com>
2019-04-15 12:29:48 +0200

    Add image export feature

--- src/main/java/de/jcm/tmod/explorer/RawImgViewer.java
+++ src/main/java/de/jcm/tmod/explorer/RawImgViewer.java
@@ -1,19 +1,32 @@
-package de.jcm.tmod.editor;
+package de.jcm.tmod.explorer;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Transparency;
+import java.awt.color.ColorSpace;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.ComponentColorModel;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferByte;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.IntBuffer;
 
+import javax.imageio.ImageIO;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileFilter;
 
 import com.google.common.io.LittleEndianDataInputStream;
 
@@ -40,14 +53,17 @@ public class RawImgViewer extends JFrame
 		int width = in.readInt();
 		int height = in.readInt();
 
-		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_BGR);
-
 		byte[] bytes = in.readNBytes(width * height * 4);
-		IntBuffer buffer = ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN).asIntBuffer();
-		int[] array = new int[width * height];
-		buffer.get(array);
 
-		image.getRaster().setDataElements(0, 0, width, height, array);
+	    int samplesPerPixel = 4; // This is the *4BYTE* in TYPE_4BYTE_ABGR
+	    int[] bandOffsets = {0, 1, 2, 3}; // This is the order (ABGR) part in TYPE_4BYTE_ABGR
+
+		DataBuffer data = new DataBufferByte(bytes, bytes.length);
+		WritableRaster raster = Raster.createInterleavedRaster(data, width, height, samplesPerPixel * width, samplesPerPixel, bandOffsets, null);
+
+		ColorModel colorModel = new ComponentColorModel(ColorSpace.getInstance(ColorSpace.CS_sRGB), true, false,
+				Transparency.TRANSLUCENT, DataBuffer.TYPE_BYTE);
+	    image = new BufferedImage(colorModel, raster, colorModel.isAlphaPremultiplied(), null);
 
 		in.close();
 	}
@@ -55,7 +71,7 @@ public class RawImgViewer extends JFrame
 	/**
 	 * Create the frame.
 	 */
-	public RawImgViewer(File file)
+	public RawImgViewer(File file, String path)
 	{
 		this.file = file;
 		try
@@ -69,6 +85,107 @@ public class RawImgViewer extends JFrame
 
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+
+		JMenu mnExportAs = new JMenu("Export as...");
+		mnFile.add(mnExportAs);
+
+		JMenuItem mntmPng = new JMenuItem("PNG");
+		mntmPng.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				JFileChooser chooser = new JFileChooser();
+				chooser.setSelectedFile(new File(path.replace(".rawimg", ".png")));
+				FileFilter filter;
+				chooser.addChoosableFileFilter(filter = new FileFilter()
+				{
+					@Override
+					public String getDescription()
+					{
+						return "PNG-Image (*.png)";
+					}
+
+					@Override
+					public boolean accept(File f)
+					{
+						return f.isDirectory() || f.getName().endsWith(".png");
+					}
+				});
+				int result = chooser.showSaveDialog(null);
+				if(result == JFileChooser.APPROVE_OPTION)
+				{
+					File target = chooser.getSelectedFile();
+					if(chooser.getFileFilter() == filter)
+					{
+						if(!target.getName().endsWith(".png"))
+						{
+							target = new File(target.getAbsolutePath() + ".png");
+						}
+					}
+					try
+					{
+						ImageIO.write(image, "png", target);
+					}
+					catch(IOException e1)
+					{
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+		mnExportAs.add(mntmPng);
+
+		JMenuItem mntmJpeg = new JMenuItem("JPEG");
+		mntmJpeg.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				JFileChooser chooser = new JFileChooser();
+				chooser.setSelectedFile(new File(path.replace(".rawimg", ".jpg")));
+				FileFilter filter;
+				chooser.addChoosableFileFilter(filter = new FileFilter()
+				{
+					@Override
+					public String getDescription()
+					{
+						return "JPEG-Image (*.jpg)";
+					}
+
+					@Override
+					public boolean accept(File f)
+					{
+						return f.isDirectory() || f.getName().endsWith(".jpg");
+					}
+				});
+				int result = chooser.showSaveDialog(null);
+				if(result == JFileChooser.APPROVE_OPTION)
+				{
+					File target = chooser.getSelectedFile();
+					if(chooser.getFileFilter() == filter)
+					{
+						if(!target.getName().endsWith(".jpg"))
+						{
+							target = new File(target.getAbsolutePath() + ".jpg");
+						}
+					}
+					try
+					{
+						ImageIO.write(image, "jpg", target);
+					}
+					catch(IOException e1)
+					{
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+		mnExportAs.add(mntmJpeg);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
